80adac6cede6725b5ffdbd4226dc774c5d79fdec
Ardulous <rohan.me2014@gmail.com>
2017-09-15 12:58:46 +0530

    Index Page

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<head>
+    <title>ConsuSys-eKYC Portal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="image assets/logo.jpg">
+    <link rel="stylesheet"type="text/css"href="stylesheets/styles.css">
+</head>
+<body>
+    <div class="fixedbox">
+        <center><img src="image assets/bobfulllogo.png"style="width:200px;"></center>
+    </div>
+    <div class="box">
+        <div class="headbox">
+            <center>
+            <img src="image assets/consusyslogo.jpg"style="width:200px;">
+            <center>
+        </div>
+        <div class="bodybox">
+            <a href="#"style="text-decoration:none;color:white;">
+                <div class="boxelement">
+                    New KYC Submission
+                </div>
+            </a>
+            <a href="#"style="text-decoration:none;color:white;">
+                <div class="boxelement">
+                    Update my KYC
+                </div>
+            </a>
+            <a href="#"style="text-decoration:none;color:white;">
+                <div class="boxelement">
+                    View my KYC
+                </div>
+            </a>
+        </div>
+    </div>
+</body>
+</html>
